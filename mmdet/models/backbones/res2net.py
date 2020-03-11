8c7f00e254b00bffe24bd21509645d9918176ed9
gasvn <nanoc@live.cn>
2020-03-11 17:18:13 +0800

    update res2net test case

--- mmdet/models/backbones/res2net.py
+++ mmdet/models/backbones/res2net.py
@@ -325,10 +325,10 @@ class Res2Net(nn.Module):
         >>> level_outputs = self.forward(inputs)
         >>> for level_out in level_outputs:
         ...     print(tuple(level_out.shape))
-        (1, 64, 8, 8)
-        (1, 128, 4, 4)
-        (1, 256, 2, 2)
-        (1, 512, 1, 1)
+        (1, 256, 8, 8)
+        (1, 512, 4, 4)
+        (1, 1024, 2, 2)
+        (1, 2048, 1, 1)
     """
 
     arch_settings = {
